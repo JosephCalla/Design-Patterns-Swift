002aef6e0a93e075bb6e2457388526cbae4f7565
Joseph Calla <35270796+JosephCalla@users.noreply.github.com>
2022-11-23 10:29:28 -0500

    Factory Method Swift

--- Design-Patterns-Swift.playground/Pages/Factory Method.xcplaygroundpage/Contents.swift
+++ Design-Patterns-Swift.playground/Pages/Factory Method.xcplaygroundpage/Contents.swift
@@ -1,7 +1,49 @@
 //: [Previous](@previous)
 
 import Foundation
+import UIKit
+// CREATIONAL
+// Factory Method
+protocol Payment {
+    func doPayment()
+}
 
-var greeting = "Hello, playground"
+class GooglePayment: Payment {
+    func doPayment() {
+        print("Haciendo el pago con GOOGLE PAYMENT")
+    }
+}
+
+class CardPayment: Payment {
+    func doPayment() {
+        print("Haciendo el pago con Tarjeta de Crédito")
+    }
+}
+
+enum TypePayment {
+    case GOOGLE
+    case CARD
+}
+
+class PaymentFactory {
+    static func buildPayment(typePayment: TypePayment) -> Payment {
+        switch typePayment {
+        case .GOOGLE:
+            return GooglePayment()
+            
+        case .CARD:
+            return CardPayment()
+        }
+    }
+}
+
+// Show Result ->
+func testFactoryMethod() {
+    var payment: Payment
+    payment = PaymentFactory.buildPayment(typePayment: .GOOGLE)
+    payment.doPayment()
+}
+
+testFactoryMethod()
 
 //: [Next](@next)
